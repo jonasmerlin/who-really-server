bb49eb99c3df6d1bed9ecfbff08d2ef94b7a09cf
Jonas Schumacher <jonas.merlin.schumacher@mailbox.org>
2017-12-09 19:22:54 +0100

    merged changes from master

--- classifiers/__init__.py
+++ classifiers/__init__.py
@@ -2,9 +2,16 @@ import os
 import glob
 import importlib
 
-# find module names in the directory
+# Find module names in the directory.
 dirs = glob.glob(os.path.dirname(os.path.abspath(__file__)) + "/*")
-module_names = [os.path.basename(m) for m in dirs if os.path.basename(m) != "__init__.py" and os.path.basename(m) != "__pycache__"]
-# import every module using importlib
+module_names = [
+                os.path.basename(m) for m in dirs
+                if os.path.basename(m) != "__init__.py"
+                and os.path.basename(m) != "__pycache__"
+                ]
 
-modules = [importlib.import_module('classifiers.' + m + '.predict') for m in module_names]
+# import every module using importlib
+modules = [
+           importlib.import_module('classifiers.' + m + '.predict')
+           for m in module_name
+           ]
--- classifiers/ethnicity/predict.py
+++ classifiers/ethnicity/predict.py
@@ -56,25 +56,3 @@ def classify(image_path):
     # get the prediction label
     print("Image ID: {}, Label: {}".format(inID, label))
     return label
-
-
-# import keras
-# from keras.models import load_model
-# from keras.applications.vgg16 import preprocess_input
-# from keras.preprocessing import image as kimage
-# import numpy as np
-# import os
-
-# tuned_model_path = os.path.join(os.path.dirname(__file__), 'tuned_model.h5')
-
-# model = load_model(tuned_model_path)
-
-# def classify(img_path):
-
-# 	img = kimage.load_img(img_path, target_size=(150, 150))
-# 	x = kimage.img_to_array(img)
-# 	x = np.expand_dims(x, axis=0)
-# 	x = preprocess_input(x)
-
-# 	print(keras.utils.np_utils.to_categorical(model.predict(x)))
-# 	return model.predict(x)
